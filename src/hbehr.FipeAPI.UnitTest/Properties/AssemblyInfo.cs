3f34677bd62be29832ce85953d822b0885f0c7e6
Henrique Behr <henriqb@gmail.com>
2017-02-05 15:10:35 +0000

    Binary for Nuget
    Builded Binary for nuget publishing !

--- src/hbehr.FipeAPI.UnitTest/Properties/AssemblyInfo.cs
+++ src/hbehr.FipeAPI.UnitTest/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("hbehr.FipeAPI.UnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Henrique Borba Behr")]
 [assembly: AssemblyProduct("hbehr.FipeAPI.UnitTest")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -16,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("798caf82-d046-40e2-a0b3-3984c534a0ba")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- src/hbehr.FipeAPI/Properties/AssemblyInfo.cs
+++ src/hbehr.FipeAPI/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -8,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("hbehr.FipeAPI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Henrique Borba Behr")]
 [assembly: AssemblyProduct("hbehr.FipeAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
